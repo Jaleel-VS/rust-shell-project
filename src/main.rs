3cddd18fe154c2a201a3792e4e3e1653d3e53a1b
Jaleel-VS <jeremikress@gmail.com>
2026-03-10 12:20:11 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{self, Write};
 
 fn main() {
+    let commands = vec!["echo", "exit", "type", ""];
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -15,6 +16,12 @@ fn main() {
             println!("{}", output);
         } else if command == "exit" {
             return;
+        } else if let Some(output) = command.strip_prefix("type ") {
+            if commands.contains(&output) {
+                println!("{}: is a shell builtin", output)
+            } else {
+                println!("{}: not found", output)
+            }
         } else {
             println!("{}: command not found", command);
         }
